54182de7b654cc9c7e575bf3b04ae084759b28cc
Sainan <sainan@calamity.gg>
2023-12-17 18:59:06 +0100

    Remove call to luaK_setoneret in TK_PIPE handler since luaK_prepcallfirstarg handles that

--- src/lparser.cpp
+++ src/lparser.cpp
@@ -2789,7 +2789,6 @@ static void expsuffix (LexState *ls, expdesc *v, int line, int flags, TypeHint *
       }
       case TK_PIPE: {  /* '|>' NAME */
         luaX_next(ls);
-        luaK_setoneret(ls->fs, v);
         expdesc func;
         singlevar(ls, &func);
         luaK_prepcallfirstarg(fs, v, &func);
